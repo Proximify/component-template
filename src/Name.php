90560e9b40d4d04912bd8a7305f7b2621586e086
Diego Macrini <macrini@proximify.ca>
2020-11-14 10:33:04 -0500

    Simpler temaple

---
+++ src/Name.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @author    Proximify Inc <support@proximify.com>
+ * @copyright Copyright (c) 2020, Proximify Inc
+ * @license   MIT
+ */
+
+namespace Proximify;
+
+class Name
+{
+}
